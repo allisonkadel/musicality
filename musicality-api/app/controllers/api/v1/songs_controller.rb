be752a5ae303908e64f7bfba562c331d634370b0
allisonkadel <owner@allisonkadel.com>
2019-01-05 08:40:43 -0800

    build songs controller create action

--- musicality-api/app/controllers/api/v1/songs_controller.rb
+++ musicality-api/app/controllers/api/v1/songs_controller.rb
@@ -3,7 +3,16 @@ class Api::V1::SongsController < ApplicationController
     def index
         @songs = Song.all
 
-        render :json => @songs, status: 200
+        render :json => @songs, :status => 200
+    end
+
+    def create
+        song = song.new(song_params)
+        if song.save
+            render :json => song, :status => 200
+        else
+            render :json => { :message => song.errors }, :status => 400
+        end
     end
 
     def show
