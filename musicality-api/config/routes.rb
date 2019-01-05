b2bdea3db321c3037fa89329ee5b7c601908b31e
allisonkadel <owner@allisonkadel.com>
2019-01-05 08:36:45 -0800

    edit songs resource routes to include 'v1' namespace

--- musicality-api/config/routes.rb
+++ musicality-api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :songs, except: [:new, :edit]
+    namespace :v1 do
+      resources :songs, except: [:new, :edit]
+    end
   end
 end
