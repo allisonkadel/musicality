8354b6bb6c595d14357274959a465abc5d239689
allisonkadel <owner@allisonkadel.com>
2019-01-06 12:26:07 -0800

    create songs and root reducers and use to create store

--- musicality-client/src/store.js
+++ musicality-client/src/store.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import {
     createStore,
-    applyMiddleware,
+    applyMiddleware, // to make asynchronous calls to backend
     combineReducers
 } from 'redux';
 import thunk from 'redux-thunk';
 
+const songsReducer = (state = [], action) => {
+    switch(action.type) {
+
+        case 'GET_SONGS_SUCCESS':
+            return action.songs
+
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    songs: songsReducer
+});
+const middleware = [thunk]
+
+export default createStore(
+    rootReducer,
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
+);
