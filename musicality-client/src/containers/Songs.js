58317fd2160899a4e18ef0b5af60ead8f0f7ba4c
allisonkadel <owner@allisonkadel.com>
2019-01-06 06:19:03 -0800

    edit App-header class styling and add to Songs Container header

--- musicality-client/src/containers/Songs.js
+++ musicality-client/src/containers/Songs.js
@@ -3,10 +3,11 @@ import './Songs.css'
 
 const Songs = (props) => (
     <div className='SongsContainer'>
-        <h1>Songs In Your Library</h1>
+        <h1 className='App-header'>Songs In Your Library</h1>
         {props.songs.map(song => 
             <div className='SongCard'>
-                <h3>{song.name} - {song.artist}</h3>
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
                 <h4>{song.chords}</h4>
                 <img width='100%'
                     src='https://www.guitar-chord.org/images/c_chord_chart.png'
