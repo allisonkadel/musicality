070082f801e98de0f7f8f321ec9bae7e9d3d16f2
allisonkadel <owner@allisonkadel.com>
2019-01-06 05:48:14 -0800

    add props to Songs

--- musicality-client/src/containers/Songs.js
+++ musicality-client/src/containers/Songs.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Songs = () => (
+const Songs = (props) => (
     <div>
-        <h3>Songs Component</h3>
+        <h1>Songs In Your Library</h1>
+        {props.songs.map(song => 
+            <div>
+                <h3>{song.name} - {song.artist}</h3>
+                <h4>{song.chords}</h4>
+                <img 
+                    src='https://www.guitar-chord.org/images/c_chord_chart.png'
+                    alt='chord chart'
+                />
+            </div>
+        )}
     </div>
 )
 
