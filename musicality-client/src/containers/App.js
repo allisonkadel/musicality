030f7de0d562019e00b6ef917c9969e88d6ad82f
allisonkadel <owner@allisonkadel.com>
2019-01-06 05:08:18 -0800

    test Songs container rendering within App container

--- musicality-client/src/containers/App.js
+++ musicality-client/src/containers/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css'
 
+const Songs = () => 
+    <div>
+        <h3>Songs Component</h3>
+    </div>
+
+
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className='App'>
                 App Container
+                <Songs/>
             </div>
         )
     }
