d78ce58c5d8df91d3dbf62d1a3c1e7f34b76d782
allisonkadel <owner@allisonkadel.com>
2019-01-06 11:23:22 -0800

    change Songs from functional to class component

--- musicality-client/src/containers/Songs.js
+++ musicality-client/src/containers/Songs.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SongCard from '../components/SongCard'
 import './Songs.css'
 
-const Songs = (props) => (
-    <div className='SongsContainer'>
-        <h1 className='App-header'>Songs In Your Library</h1>
-        {props.songs.map(song => <SongCard key={song.id} song={song}/>)}
-    </div>
-)
+class Songs extends Component {
+
+    render() {
+        return(
+            <div className='SongsContainer'>
+                <h1 className='App-header'>Songs In Your Library</h1>
+                {this.props.songs.map(song => <SongCard key={song.id} song={song}/>)}
+            </div>
+        )}
+}
 
 export default Songs;
