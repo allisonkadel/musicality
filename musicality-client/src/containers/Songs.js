cdc890321ce627facde405b2c3d180f7e2f8749b
allisonkadel <owner@allisonkadel.com>
2019-01-06 06:03:41 -0800

    add styling to SongsCard

--- musicality-client/src/containers/Songs.js
+++ musicality-client/src/containers/Songs.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './Songs.css'
 
 const Songs = (props) => (
     <div>
         <h1>Songs In Your Library</h1>
         {props.songs.map(song => 
-            <div>
+            <div className='SongCard'>
                 <h3>{song.name} - {song.artist}</h3>
                 <h4>{song.chords}</h4>
-                <img 
+                <img width='100%'
                     src='https://www.guitar-chord.org/images/c_chord_chart.png'
                     alt='chord chart'
                 />
