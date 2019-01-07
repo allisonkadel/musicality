ef83e4a6eb568a96569afb77a6e87e2d1113e401
allisonkadel <owner@allisonkadel.com>
2019-01-07 11:04:34 -0800

    build out ChordsCard

--- musicality-client/src/components/chords/ChordsCard.js
+++ musicality-client/src/components/chords/ChordsCard.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ChordCard from './ChordCard';
 
 const ChordsCard = ({chords}) => (
-    <div key={chord.id}>
-
-        <ChordCard/>
-        <img width='100%'
-            src='https://www.guitar-chord.org/images/c_chord_chart.png'
-            alt='chord chart'
-        />
+    <div>
+        {chords.map(chord => <ChordCard chord={chord}/>)}
     </div>
 )
-export default SongCard;
+export default ChordsCard;
