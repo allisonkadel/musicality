2452d8842ac52808d93ba72f5b7878c5f768714d
allisonkadel <owner@allisonkadel.com>
2019-01-07 05:31:20 -0800

    add input fields for songForm

--- musicality-client/src/containers/SongForm.js
+++ musicality-client/src/containers/SongForm.js
@@ -3,10 +3,35 @@ import { connect } from 'react-redux';
 
 class SongForm extends Component {
     render() {
+        const { title, artist, chords } = this.props.songFormData;
         return (
             <div>
-                Add a New Song
+                Add a Song to Your Library
                 <form>
+                    <div>
+                        <label>Title</label>
+                        <input
+                            type='text'
+                            name='title'
+                            value={title}
+                        />
+                    </div>
+                    <div>
+                        <label>Artist</label>
+                        <input
+                            type='text'
+                            name='artist'
+                            value={artist}
+                        />
+                    </div>
+                    <div>
+                        <label>Chords</label>
+                        <input
+                            type='text'
+                            name='chords'
+                            value={chords}
+                        />
+                    </div>
                 </form>
             </div>
         )
