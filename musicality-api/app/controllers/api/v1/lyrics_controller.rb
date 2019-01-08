1c15bfc5db27aa563fa823e90d4d0acb36384336
allisonkadel <owner@allisonkadel.com>
2019-01-08 09:24:23 -0800

    build index and create actions for lyrics controller

--- musicality-api/app/controllers/api/v1/lyrics_controller.rb
+++ musicality-api/app/controllers/api/v1/lyrics_controller.rb
@@ -3,52 +3,39 @@ class Api::V1::LyricsController < ApplicationController
     before_action :set_lyric, :only => [:show, :update, :destroy]
 
     def index
-        @lyrics = Lyric.all
+        song = Song.find(params[:song_id])
+        lyrics = song.lyrics
 
-        render :json => @lyrics, :status => 200
+        render :json => lyrics, :status => 200
     end
 
     def create
-        song = Song.new(song_params)
-        if song.save
-            render :json => song, :status => 200
+        lyric = Lyric.new(:chord => params[:chord],:text => params[:text],:song_id => params[:song_id])
+        if lyric.save
+            render :json => lyric, :status => 200
         else
-            render :json => { :message => song.errors }, :status => 400
+            render :json => { :message => lyric.errors }, :status => 400
         end
     end
 
-    def show
-        render :json => @song, status: 200
-    end
+    # def update
+    #     if @song.update(song_params)
+    #         render :json => @song, :status => 200
+    #     else
+    #         render :json => { :message => song.errors }, :status => 400
+    #     end
+    # end
 
-    def update
-        if @song.update(song_params)
-            render :json => @song, :status => 200
-        else
-            render :json => { :message => song.errors }, :status => 400
-        end
-    end
-
-    def destroy
-        binding.pry
-        if @song.destroy
-            render :status => 204
-        else
-            render :json => { 
-                :songId => @song.id,
-                :message => 'Unable to remove song' 
-                }, :status => 400
-        end
-    end
-
-    private
-
-    def song_params
-        params.require(:song).permit(:name,:artist,:chords)
-    end
-
-    def set_song
-        @song = Song.find(params[:id])
-    end
+    # def destroy
+    #     binding.pry
+    #     if @song.destroy
+    #         render :status => 204
+    #     else
+    #         render :json => { 
+    #             :songId => @song.id,
+    #             :message => 'Unable to remove song' 
+    #             }, :status => 400
+    #     end
+    # end
 
 end
