32e80edd7a895f2ed45a36b5cc79215d154f414c
allisonkadel <owner@allisonkadel.com>
2019-01-08 09:03:13 -0800

    create lyrics resource nested within songs

--- musicality-api/config/routes.rb
+++ musicality-api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :songs, except: [:new, :edit]
+      resources :songs, except: [:new, :edit] do
+        resources :lyrics, except: [:new, :edit]
+      end
     end
   end
 end
