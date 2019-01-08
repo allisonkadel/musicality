275d6c7bffe3dfd72a7fc7a472f2638836aef957
allisonkadel <owner@allisonkadel.com>
2019-01-07 16:16:10 -0800

    update

--- musicality-client/src/components/SongCard.js
+++ musicality-client/src/components/SongCard.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
+// import ChordsCard from './chords/ChordsCard';
+import LyricContainer from '../containers/LyricContainer';
 
-const SongCard = ({song}) => (
-    <div key={song.id} className='SongCard'>
-        <h3>{song.name}</h3>
-        <h4>{song.artist}</h4>
-        <h4>{song.chords}</h4>
-        <img width='100%'
-            src='https://www.guitar-chord.org/images/c_chord_chart.png'
-            alt='chord chart'
-        />
-    </div>
-)
+class SongCard extends Component {
+    debugger
+
+    // handleOnClick() {
+    //     debugger
+    //     this.props.destroySong(this.props.song.id)
+    // }
+
+    render() {
+        return(
+            <div key={this.props.song.id} className='SongCard'>
+                <h3>{this.props.song.name}</h3>
+                <h4>{this.props.song.artist}</h4>
+                <strong>This is where the LyricContainer goes</strong>
+                <img width='100%'
+                    src='https://www.guitar-chord.org/images/c_chord_chart.png'
+                    alt='chord chart'
+                />
+                {/* <LyricContainer sondId={song.id}/> */}
+                <button onClick={this.props.destroySong(this.props.song.id)}>X</button>
+            </div>
+        )
+    }
+
+}
 export default SongCard;
