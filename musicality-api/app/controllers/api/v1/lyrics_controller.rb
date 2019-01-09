4e90d1f89c8fb3a6bf00e12e5656d610fc32986f
allisonkadel <owner@allisonkadel.com>
2019-01-09 12:28:42 -0800

    up to date

--- musicality-api/app/controllers/api/v1/lyrics_controller.rb
+++ musicality-api/app/controllers/api/v1/lyrics_controller.rb
@@ -1,18 +1,6 @@
 class Api::V1::LyricsController < ApplicationController
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     before_action :set_song, :only => [:show, :create, :destroy]
-=======
-    before_action :set_song, :only => [:show, :create, :update, :destroy]
->>>>>>> parent of 0b758fb... Revert "build destroy route tests for song lyrics"
-=======
-    before_action :set_lyric, :only => [:show, :update, :destroy]
->>>>>>> parent of 1335dc1... build destroy route tests for song lyrics
-=======
-    before_action :set_lyric, :only => [:show, :update, :destroy]
->>>>>>> parent of 1335dc1... build destroy route tests for song lyrics
 
     def index
         song = Song.find(params[:song_id])
@@ -37,50 +25,23 @@ class Api::V1::LyricsController < ApplicationController
     #     end
     # end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     def destroy
-<<<<<<< HEAD
-        binding.pry
-        if @song.destroy
-            render :status => 204
-        else
-            render :json => { 
-                :songId => @song.id,
-                :message => 'Unable to remove song' 
-                }, :status => 400
-=======
         lyric = @song.lyrics.find(params[:id])
         if lyric.destroy
             render :status => 204
         else
             error_response(lyric)
->>>>>>> parent of 0b758fb... Revert "build destroy route tests for song lyrics"
         end
     end
-=======
-=======
->>>>>>> parent of 1335dc1... build destroy route tests for song lyrics
-    # def destroy
-    #     binding.pry
-    #     if @song.destroy
-    #         render :status => 204
-    #     else
-    #         render :json => { 
-    #             :songId => @song.id,
-    #             :message => 'Unable to remove song' 
-    #             }, :status => 400
-    #     end
-    # end
-<<<<<<< HEAD
->>>>>>> parent of 1335dc1... build destroy route tests for song lyrics
-=======
->>>>>>> parent of 1335dc1... build destroy route tests for song lyrics
 
     private
 
     def set_song
         @song = Song.find(params[:song_id])
+    end
+
+    def lyric_params
+        params.require(:lyric).permit(:text, :chord)
     end
 
 end
