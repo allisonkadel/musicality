5888f261ae374e7d45940a5661d543446a5e82d8
allisonkadel <owner@allisonkadel.com>
2019-01-11 14:06:07 -0800

    add image to guitar chord chart

--- musicality-client/src/components/ChordChart.js
+++ musicality-client/src/components/ChordChart.js
@@ -3,7 +3,8 @@ import React from 'react';
 const ChordChart = () => {
     return(
         <div className='App-header'>
-            <h1>Chord Chart images will go here.</h1>
+            <h1>Guitar Chord Chart</h1>
+            <img src="https://i.pinimg.com/originals/91/f7/37/91f7378a9728ff1842e542bb820cdd39.gif"/>
         </div>
     )
 }
